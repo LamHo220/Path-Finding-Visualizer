94cca036094979c454633c5ac7f86fdf7e90b063
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-02 23:34:26 +0800

    Create Buttons.jsx

---
+++ src/Main/Buttons.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StartButton = (props) => {
+  const { buttonName, setStart } = props;
+  return (
+    <div className="relative">
+      <button
+        type="button"
+        className="bg-sky-200 text-gray-700 transition ease-in-out delay-150 hover:scale-105 duration-200 px-5 py-1 group bg-white rounded-md inline-flex items-center text-base font-medium"
+        aria-expanded="false"
+        onClick={() => setStart()}
+      >
+        <span>{buttonName}</span>
+      </button>
+    </div>
+  );
+};
+
+const TrueFalseButton = (props) => {
+  const { buttonName, flag, setFlag } = props;
+  return (
+    <div className="relative">
+      <button
+        type="button"
+        className={`${
+          flag ? "bg-green-300" : "bg-rose-300"
+        } text-gray-700 transition ease-in-out delay-150 hover:scale-105 duration-300 px-5 py-1 group bg-white rounded-md inline-flex items-center text-base font-medium`}
+        aria-expanded="false"
+        onClick={() => {
+          setFlag();
+        }}
+      >
+        <span>{buttonName}</span>
+      </button>
+    </div>
+  );
+};
+
+const SelectionButton = (props) => {
+  const { buttonName, icon } = props;
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (open && ref.current && !ref.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [open]);
+  return (
+    <div className="relative" ref={ref}>
+      <button
+        type="button"
+        className="text-gray-400 px-5 py-1 group bg-white rounded-md inline-flex items-center text-base font-medium"
+        aria-expanded="false"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <span>{buttonName}</span>
+        {icon && <FontAwesomeIcon icon={icon} />}
+      </button>
+      {open && props.children}
+    </div>
+  );
+};
+
+export { SelectionButton, StartButton, TrueFalseButton };
