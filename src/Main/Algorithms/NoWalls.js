b169d36124544a3416931524fc6dbdb9788796e1
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-08 15:02:25 +0800

    Update NoWalls.js

--- src/Main/Algorithms/NoWalls.js
+++ src/Main/Algorithms/NoWalls.js
@@ -1,3 +1,5 @@
+import { changeClassName } from "../misc/misc";
+
 const NoWalls = async (grid) => {
   for (let row of grid) {
     for (let node of row) {
@@ -5,15 +7,7 @@ const NoWalls = async (grid) => {
         continue;
       }
       node.isWall = false;
-      document.getElementById(`${node.row}-${node.col}`).className = `node ${
-        node.isStart
-          ? "bg-emerald-400"
-          : node.isEnd
-          ? "bg-pink-400"
-          : node.isWall
-          ? "bg-gray-400"
-          : ""
-      } border border-gray-200 m-0 p-0`;
+      changeClassName(node);
     }
   }
 };
