b4bb557b0316aec719db2b8236bfb865313a2c02
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-08 15:03:06 +0800

    Update misc.js

--- src/Main/misc/misc.js
+++ src/Main/misc/misc.js
@@ -1,13 +1,15 @@
 const rowDir = { N: -1, S: 1, E: 0, W: 0 };
 const colDir = { N: 0, S: 0, E: 1, W: -1 };
 
-const changeClassName = async (node, name) => {
+const changeClassName = async (node, name="", fakeIsWall=false) => {
   const element = document.getElementById(`${node.row}-${node.col}`);
   element.className = `node ${
     node.isStart
       ? "bg-emerald-400"
       : node.isEnd
       ? "bg-pink-400"
+      : fakeIsWall
+      ? name
       : node.isWall
       ? "bg-gray-400"
       : name
@@ -17,33 +19,33 @@ const changeClassName = async (node, name) => {
 const buildBoundaries = async (grid, duration) => {
   const maxRow = grid.length;
   const maxCol = grid[0].length;
-  for (let i = 1; i < maxRow - 1; ++i) {
+  for (let i = 0; i < maxRow; ++i) {
     setTimeout(() => {
       let node = grid[i][0];
       if (!(node.isStart || node.isEnd)) {
         node.isWall = true;
-        changeClassName(node, "");
+        changeClassName(node);
       }
       node = grid[i][maxCol - 1];
       if (!(node.isStart || node.isEnd)) {
         node.isWall = true;
-        changeClassName(node, "");
+        changeClassName(node);
       }
-    }, i * 0);
+    }, duration * i);
   }
-  for (let i = 0; i < maxCol; ++i) {
+  for (let j = 0; j < maxCol; ++j) {
     setTimeout(() => {
-      let node = grid[0][i];
+      let node = grid[0][j];
       if (!(node.isStart || node.isEnd)) {
         node.isWall = true;
-        changeClassName(node, "");
+        changeClassName(node);
       }
-      node = grid[maxRow - 1][i];
+      node = grid[maxRow - 1][j];
       if (!(node.isStart || node.isEnd)) {
         node.isWall = true;
-        changeClassName(node, "");
+        changeClassName(node);
       }
-    }, i * 0);
+    }, duration * j);
   }
 };
 
@@ -66,22 +68,22 @@ const refresh = (visitedNodes, shortestPath) => {
 
 const rand = (from, to) => {
   return Math.floor(Math.random() * (to - from + 1) + from);
-}
+};
 
-const reverseGrid = (grid,maxRow,maxCol) => {
+const reverseGrid = (grid, maxRow, maxCol) => {
   for (let row = 0; row < maxRow; ++row) {
     for (let col = 0; col < maxCol; ++col) {
       setTimeout(() => {
         let node = grid[row][col];
         if (!(node.isStart || node.isEnd)) {
           node.isWall = !node.isWall;
-          changeClassName(node,"");
+          changeClassName(node);
         }
       }, 30 * (row + col));
     }
   }
-  return (maxRow-1)*(maxCol-1);
-}
+  return (maxRow - 1) * (maxCol - 1);
+};
 
 const isHorizontalCut = (width, height) => {
   if (width < height) return true;
@@ -89,5 +91,20 @@ const isHorizontalCut = (width, height) => {
   return Math.floor(Math.random() * 2) === 1;
 };
 
+const delay = (t)=>{
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve,t);
+  });
+}
 
-export { changeClassName, buildBoundaries, refresh, rand, reverseGrid, rowDir, colDir,isHorizontalCut };
+export {
+  changeClassName,
+  buildBoundaries,
+  refresh,
+  rand,
+  reverseGrid,
+  rowDir,
+  colDir,
+  isHorizontalCut,
+  delay,
+};
