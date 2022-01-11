2eacb7ab13e3bfa5fd2c109d944f26b2c734b8e6
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-11 18:10:25 +0800

    Create RecursiveBacktracking.js

---
+++ src/Main/Maze/RecursiveBacktracking.js
@@ -0,0 +1,84 @@
+import { rand, changeClassName, rowDir, colDir, delay } from "../misc/misc";
+
+const deltaTime = 60;
+
+const RecursiveBacktrackingMaze = async (
+  grid,
+  maxRow,
+  maxCol,
+  startNode,
+  endNode
+) => {
+  startNode.isWall = true;
+  endNode.isWall = true;
+
+  let randCol = Math.floor(rand(0, maxCol - 1) / 2) * 2 + 1;
+  let randRow = Math.floor(rand(0, maxRow - 1) / 2) * 2 + 1;
+  let node = grid[randRow][randCol];
+  node.isWall=false;
+  changeClassName(node);
+  doRecursiveBacktracker(grid, node, maxRow, maxCol);
+  startNode.isWall = false;
+  endNode.isWall = false;
+};
+
+const getDirection = (grid, row, col) => {
+  let res = [];
+  if (row > 2 && grid[row - 2][col].isWall) {
+    res.push("N");
+  }
+  if (row + 2 < grid.length && grid[row + 2][col].isWall) {
+    res.push("S");
+  }
+  if (col > 2 && grid[row][col - 2].isWall) {
+    res.push("W");
+  }
+  if (col + 2 < grid[0].length && grid[row][col + 2].isWall) {
+    res.push("E");
+  }
+  return res;
+};
+
+const doRecursiveBacktracker = async (grid, node, maxRow, maxCol) => {
+  let row = node.row;
+  let col = node.col;
+
+  let dir = getDirection(grid, row, col);
+  while (!!dir.length) {
+    let dirId = rand(0, dir.length - 1);
+    let direction = dir[dirId];
+    dir = dir.filter((e) => e !== direction);
+    let nextRow = row + rowDir[direction];
+    let nextCol = col + colDir[direction];
+
+    let nextNode = grid[nextRow][nextCol];
+
+    let pretendRow = nextRow + rowDir[direction];
+    let pretendCol = nextCol + colDir[direction];
+    if (
+      pretendRow < 0 ||
+      pretendRow >= maxRow ||
+      pretendCol < 0 ||
+      pretendCol >= maxCol
+    ) {
+      continue;
+    }
+    if (!grid[pretendRow][pretendCol].isWall ) {
+      continue;
+    }
+    if (!(nextNode.isStart || nextNode.isEnd)) {
+      nextNode.isWall = false;
+      changeClassName(nextNode, "");
+      await delay(deltaTime);
+    }
+    nextNode = grid[pretendRow][pretendCol];
+    if (!(nextNode.isStart || nextNode.isEnd)) {
+      nextNode.isWall = false;
+      changeClassName(nextNode, "");
+      await delay(deltaTime);
+    }
+    await doRecursiveBacktracker(grid, nextNode, maxRow, maxCol);
+  }
+};
+
+export default RecursiveBacktrackingMaze;
