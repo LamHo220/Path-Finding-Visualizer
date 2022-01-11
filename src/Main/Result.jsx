0ab4d7b4556809d5ee0d3bbd678de295cad556ec
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-11 18:09:17 +0800

    Update Result.jsx

--- src/Main/Result.jsx
+++ src/Main/Result.jsx
@@ -1,16 +1,16 @@
 const Result = (props) => {
   return (
-    <div className="flex md:justify-around items-center">
-      <div className="font-medium text-gray-700">
+    <div className="flex md:justify-around items-center dark:bg-gray-900">
+      <div className="font-medium text-gray-700 dark:text-gray-400">
         Algorithm: {props.algorithm}
       </div>
-      <div className="font-medium text-gray-700">
+      <div className="font-medium text-gray-700 dark:text-gray-400">
         Heuristic: {props.heuristic}
       </div>
-      <div className="font-noraml text-gray-700">
+      <div className="font-noraml text-gray-700 dark:text-gray-400">
         Number of Steps: {props.steps}
       </div>
-      <div className="font-noraml text-gray-700">
+      <div className="font-noraml text-gray-700 dark:text-gray-400">
         Length of Path: {props.pathLength}
       </div>
     </div>
