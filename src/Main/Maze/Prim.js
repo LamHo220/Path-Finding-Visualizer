74af3391ce741613a9ca6754dfda153e4146fd75
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-11 18:10:16 +0800

    Create Prim.js

---
+++ src/Main/Maze/Prim.js
@@ -0,0 +1,134 @@
+import { rand, changeClassName, rowDir, colDir, delay } from "../misc/misc";
+
+const deltaTime = 60;
+
+const PrimMaze = async (grid, maxRow, maxCol) => {
+  let isStarted = false;
+  let row = Math.floor(rand(maxRow / 3, maxRow - 2) / 2) * 2 + 1;
+  let col = Math.floor(rand(maxCol / 2, maxCol - 2) / 2) * 2 + 1;
+  let pretendNodes = [grid[row][col]];
+  while (!!pretendNodes.length) {
+    let current = pretendNodes[rand(0, pretendNodes.length - 1)];
+    if (isStarted) {
+      await connectTwoNode(grid, current, maxRow, maxCol);
+    }
+    pretendNodes = pretendNodes.filter((e) => e !== current);
+    let crow = current.row;
+    let ccol = current.col;
+    if (!(current.isStart || current.isEnd)) {
+      if (!(current.isStart || current.isEnd)) {
+        current.isWall = false;
+      }
+      changeClassName(grid[crow][ccol]);
+      await delay(deltaTime);
+      let dir = await getPrimDir(grid, current, maxRow, maxCol);
+      if (dir.length !== 0) {
+        for (let direction of dir) {
+          let col = ccol + colDir[direction] * 2;
+          let row = crow + rowDir[direction] * 2;
+          pretendNodes.push(grid[row][col]);
+        }
+      }
+    }
+    isStarted = true;
+  }
+};
+
+const connectTwoNode = async (grid, toNode, maxRow, maxCol) => {
+  let toRow = toNode.row;
+  let toCol = toNode.col;
+  let fromNode;
+  let fromArr = [];
+  let tempNode;
+  if (toRow - 2 >= 0) {
+    tempNode = grid[toRow - 2][toCol];
+    if (!tempNode.isWall && !tempNode.isStart && !tempNode.isEnd)
+      fromArr.push(tempNode);
+  }
+  if (toRow + 2 < maxRow) {
+    tempNode = grid[toRow + 2][toCol];
+    if (!tempNode.isWall && !tempNode.isStart && !tempNode.isEnd)
+      fromArr.push(tempNode);
+  }
+  if (toCol + 2 < maxCol) {
+    tempNode = grid[toRow][toCol + 2];
+    if (!tempNode.isWall && !tempNode.isStart && !tempNode.isEnd)
+      fromArr.push(tempNode);
+  }
+  if (toCol - 2 >= 0) {
+    tempNode = grid[toRow][toCol - 2];
+    if (!tempNode.isWall && !tempNode.isStart && !tempNode.isEnd)
+      fromArr.push(tempNode);
+  }
+  fromNode = fromArr[rand(0, fromArr.length - 1)];
+  let fromRow = fromNode.row;
+  let fromCol = fromNode.col;
+  if (!(toNode.isEnd || toNode.isStart)) {
+    let next;
+    if (toRow - fromRow === 2) {
+      // South
+      next = grid[fromRow + 1][fromCol];
+    } else if (toCol - fromCol === 2) {
+      // East
+      next = grid[fromRow][fromCol + 1];
+    } else if (toRow - fromRow === -2) {
+      // North
+      next = grid[fromRow - 1][fromCol];
+    } else if (toCol - fromCol === -2) {
+      // West
+      next = grid[fromRow][fromCol - 1];
+    }
+    if (!(next.isStart || next.isEnd)) {
+      next.isWall = false;
+      changeClassName(next);
+    }
+    await delay(deltaTime);
+    if (!(toNode.isStart || toNode.isEnd)) {
+      toNode.isWall = false;
+      changeClassName(toNode);
+    }
+    await delay(deltaTime);
+  }
+};
+
+const getPrimDir = async (grid, node, maxRow, maxCol) => {
+  let row = node.row;
+  let col = node.col;
+  let result = [];
+  let next;
+  if (row - 2 >= 0) {
+    next = grid[row - 2][col];
+    if (next.isWall && !next.isStart && !next.isEnd) {
+      result.push("N");
+      changeClassName(grid[row - 2][col], "bg-cyan-500", true);
+      await delay(deltaTime);
+    }
+  }
+  if (row + 2 < maxRow) {
+    next = grid[row + 2][col];
+    if (next.isWall && !next.isStart && !next.isEnd) {
+      result.push("S");
+      changeClassName(grid[row + 2][col], "bg-cyan-500", true);
+      await delay(deltaTime);
+    }
+  }
+  if (col + 2 < maxCol) {
+    next = grid[row][col + 2];
+    if (next.isWall && !next.isStart && !next.isEnd) {
+      result.push("E");
+      changeClassName(grid[row][col + 2], "bg-cyan-500", true);
+      await delay(deltaTime);
+    }
+  }
+  if (col - 2 >= 0) {
+    next = grid[row][col - 2];
+    if (next.isWall && !next.isStart && !next.isEnd) {
+      result.push("W");
+      changeClassName(grid[row][col - 2], "bg-cyan-500", true);
+      await delay(deltaTime);
+    }
+  }
+  return result;
+};
+
+export default PrimMaze;
