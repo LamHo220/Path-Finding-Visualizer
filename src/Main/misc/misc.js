0cba5944f5859c2495db6d3c8bd3965d4bd9a41f
Chun Ho Lam <58981086+LamHo220@users.noreply.github.com>
2022-01-11 18:10:30 +0800

    Update misc.js

--- src/Main/misc/misc.js
+++ src/Main/misc/misc.js
@@ -1,3 +1,6 @@
+import aStar from "../Algorithms/PathFinding/AStar";
+import dijkstra from "../Algorithms/PathFinding/Dijkstra";
+
 const rowDir = { N: -1, S: 1, E: 0, W: 0 };
 const colDir = { N: 0, S: 0, E: 1, W: -1 };
 
@@ -5,84 +8,44 @@ const changeClassName = async (node, name="", fakeIsWall=false) => {
   const element = document.getElementById(`${node.row}-${node.col}`);
   element.className = `node ${
     node.isStart
-      ? "bg-emerald-400"
+      ? "bg-blue-400 dark:bg-blue-700"
       : node.isEnd
-      ? "bg-pink-400"
+      ? "bg-pink-400  dark:bg-pink-700"
       : fakeIsWall
       ? name
       : node.isWall
-      ? "bg-gray-400"
+      ? "bg-gray-400 dark:bg-gray-700"
       : name
-  } border border-gray-200 m-0 p-0`;
+  } border border-gray-200 m-0 p-0 dark:border-gray-700 hover:bg-orange-300`;
 };
 
-const buildBoundaries = async (grid, duration) => {
-  const maxRow = grid.length;
-  const maxCol = grid[0].length;
-  for (let i = 0; i < maxRow; ++i) {
-    setTimeout(() => {
-      let node = grid[i][0];
-      if (!(node.isStart || node.isEnd)) {
-        node.isWall = true;
-        changeClassName(node);
-      }
-      node = grid[i][maxCol - 1];
-      if (!(node.isStart || node.isEnd)) {
-        node.isWall = true;
-        changeClassName(node);
-      }
-    }, duration * i);
+const clearPath = async (grid) => {
+  for (let row of grid) {
+    for (let node of row) {
+      changeClassName(node);
+    }
   }
-  for (let j = 0; j < maxCol; ++j) {
-    setTimeout(() => {
-      let node = grid[0][j];
-      if (!(node.isStart || node.isEnd)) {
-        node.isWall = true;
-        changeClassName(node);
-      }
-      node = grid[maxRow - 1][j];
-      if (!(node.isStart || node.isEnd)) {
-        node.isWall = true;
-        changeClassName(node);
-      }
-    }, duration * j);
-  }
-};
+}
 
-const refresh = (visitedNodes, shortestPath) => {
+const refresh = async (visitedNodes, shortestPath) => {
   const n = visitedNodes.length;
   for (let i = 0; i < n; ++i) {
     const node = visitedNodes[i];
     if (!(node.isStart || node.isEnd)) {
-      changeClassName(node, "bg-cyan-300");
+      changeClassName(node, "bg-cyan-300 dark:bg-cyan-700");
     }
   }
   const m = shortestPath.length;
   for (let i = 0; i < m; ++i) {
     const node = shortestPath[i];
     if (!(node.isStart || node.isEnd)) {
-      changeClassName(node, "bg-yellow-300");
+      changeClassName(node, "bg-yellow-300 dark:bg-yellow-700");
     }
   }
 };
 
 const rand = (from, to) => {
   return Math.floor(Math.random() * (to - from + 1) + from);
-};
-
-const reverseGrid = (grid, maxRow, maxCol) => {
-  for (let row = 0; row < maxRow; ++row) {
-    for (let col = 0; col < maxCol; ++col) {
-      setTimeout(() => {
-        let node = grid[row][col];
-        if (!(node.isStart || node.isEnd)) {
-          node.isWall = !node.isWall;
-          changeClassName(node);
-        }
-      }, 30 * (row + col));
-    }
-  }
-  return (maxRow - 1) * (maxCol - 1);
 };
 
 const isHorizontalCut = (width, height) => {
@@ -97,14 +60,15 @@ const delay = (t)=>{
   });
 }
 
+
 export {
   changeClassName,
-  buildBoundaries,
   refresh,
   rand,
-  reverseGrid,
-  rowDir,
-  colDir,
+  //reverse,
   isHorizontalCut,
   delay,
+  clearPath,
+  rowDir,
+  colDir
 };
